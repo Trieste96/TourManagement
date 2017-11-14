afa6419e264f550e5c8e3e0c1d44d949b7a52c96
DESKTOP-GRJIQGI/e <truongminhtriet96@gmail.com>
2017-11-14 13:54:25 +0700

    Add project files.

---
+++ TourMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TourMVC.Startup))]
+namespace TourMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
