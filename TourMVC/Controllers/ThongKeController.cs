d8b3e89f7fb7987cac56b48423aec48b854eeaf6
DESKTOP-GRJIQGI/e <truongminhtriet96@gmail.com>
2017-12-17 22:34:45 +0700

    Chuyển Entity từ project MVC sang TourModel

---
+++ TourMVC/Controllers/ThongKeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourModel;
+using System.Data.Entity;
+
+namespace TourMVC.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        TourDBEntities db = new TourDBEntities();
+        // GET: ThongKe
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: ThongKe/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ThongKe/Create
+        public ActionResult SoDoan()
+        {
+            ViewBag.Title = "Thống kê số đoàn";
+            return View();
+        }
+
+        // POST: ThongKe/Create
+        [HttpPost]
+        public ActionResult SoDoan(DateTime TuNgayGio, DateTime DenNgayGio)
+        {
+            // TODO: Add insert logic here
+            ViewBag.TuNgay = TuNgayGio.ToString("dd-MM-yyyy");
+            ViewBag.DenNgay = DenNgayGio.ToString("dd-MM-yyyy");
+            ViewBag.LoaiCP = db.LoaiChiPhis.ToList();
+            return View();
+        }
+
+        // GET: ThongKe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ThongKe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ThongKe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ThongKe/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TourModel/TourModel.Designer.cs
+++ TourModel/TourModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\TAILIEUCNTT\XÂY DỰNG PM THEO MÔ HÌNH PHÂN LỚP\Binh\TourManagement\TourModel\TourModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\e\documents\visual studio 2015\Projects\TourManagement\TourModel\TourModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
